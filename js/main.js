b0a05e6b4dbf9182bfe20afa1efeac8432b17fc4
krasznai.valentin <krasznai.valentin@openminds.hu>
2016-03-10 11:38:58 +0100

    update
    sasdf

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by OPM-285 on 2016. 03. 10..
+ */
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('sw.js').then(function(reg) {
+        console.log(':^)', reg);
+        // TODO
+    }).catch(function(err) {
+        console.log(':^(', err);
+    });
+}
