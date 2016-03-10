94b48efb3dc35769808fd3f35560473c3a75450e
krasznai.valentin <krasznai.valentin@openminds.hu>
2016-03-10 14:09:20 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 if ('serviceWorker' in navigator) {
     console.log('Service Worker is supported');
-    navigator.serviceWorker.register('sw.js').then(function(reg) {
-        console.log(':^)', reg);
-
-        reg.pushManager.subscribe({
-            userVisibleOnly: true
-        }).then(function(sub) {
-            console.log('endpoint:', sub.endpoint);
+    navigator.serviceWorker.register('sw.js')
+        .then(initialiseState)
+        .catch(function (err) {
+            console.log('Error: ', err);
         });
+}
 
-    }).catch(function(err) {
-        console.log(':^(', err);
-    });
+// Once the service worker is registered set the initial state
+function initialiseState(reg) {
+    console.log('Success: ', reg);
+
+    // Are Notifications supported in the service worker?
+    if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+        console.warn('Notifications aren\'t supported.');
+    }
+
+    // Check the current Notification permission.
+    // If its denied, it's a permanent block until the
+    // user changes the permission
+    if (Notification.permission === 'denied') {
+        console.warn('The user has blocked notifications.');
+    }
+
+    // Check if push messaging is supported
+    if (!('PushManager' in window)) {
+        console.warn('Push messaging isn\'t supported.');
+    }
 }
