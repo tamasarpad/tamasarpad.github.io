1992d62319597ffecaf6d5be9b9a209c73dce7a9
krasznai.valentin <krasznai.valentin@openminds.hu>
2016-03-10 12:35:46 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker is supported');
     navigator.serviceWorker.register('sw.js').then(function(reg) {
         console.log(':^)', reg);
-        reg.pushManager.subscribe({
-            userVisibleOnly: true
-        }).then(function(sub) {
-            console.log('endpoint:', sub.endpoint);
-        });
+        //reg.pushManager.subscribe({
+        //    userVisibleOnly: true
+        //}).then(function(sub) {
+        //    console.log('endpoint:', sub.endpoint);
+        //});
     }).catch(function(err) {
         console.log(':^(', err);
     });
