d3ec8d24b7779ceac56e8c2dfc39a5ba927079b6
krasznai.valentin <krasznai.valentin@openminds.hu>
2016-03-11 09:24:07 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -86,7 +86,7 @@ function subscribe() {
     pushButton.disabled = true;
 
     navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-        serviceWorkerRegistration.pushManager.subscribe()
+        serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true})
             .then(function(subscription) {
                 // The subscription was successful
                 isPushEnabled = true;
