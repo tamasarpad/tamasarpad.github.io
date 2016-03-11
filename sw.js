240f563eeb7a831a17bd1c31b9e4bed09bf4040b
krasznai.valentin <krasznai.valentin@openminds.hu>
2016-03-11 09:15:38 +0100

    asd

--- sw.js
+++ sw.js
@@ -6,7 +6,23 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
     console.log('Service worker activated', event);
 });
+//self.addEventListener('push', function(event) {
+//    console.log('Push message received', event);
+//    // TODO
+//});
 self.addEventListener('push', function(event) {
-    console.log('Push message received', event);
-    // TODO
+    console.log('Received a push message', event);
+
+    var title = 'Yay a message.';
+    var body = 'We have received a push message.';
+    var icon = '/images/icon-192x192.png';
+    var tag = 'simple-push-demo-notification-tag';
+
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: tag
+        })
+    );
 });
