1773457d72445f29f0e8d61afdffeb1493dae6dd
krasznai.valentin <krasznai.valentin@openminds.hu>
2016-03-25 10:23:50 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -20,11 +20,11 @@ window.addEventListener('load', function() {
 });
 
 window.addEventListener('streamysubscribeerror', function(e) {
-    console.log(e.detail);
+    console.log(e);
     alert("Subscribe error! " + e.detail.errorMessage);
 });
 
 window.addEventListener('streamysubscribesucces', function(e) {
-    console.log(e.detail);
+    console.log(e);
     alert("Subscribe successful! Subscribe Id " + e.detail.subscriptionId);
 });
