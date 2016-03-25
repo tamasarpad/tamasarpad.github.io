71a8f332c78c178c7a9d807f0b39290252c35af5
krasznai.valentin <krasznai.valentin@openminds.hu>
2016-03-25 10:14:26 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
+var isInt = function(value) {
+    var x;
+    if (isNaN(value)) {
+        return false;
+    }
+    x = parseFloat(value);
+    return (x | 0) === x;
+};
+
 window.addEventListener('load', function() {
     var button = $('#button');
     var siteId = $('#siteId');
     button.click(function () {
-        StreamyAPI.onSubscribe(siteId.val());
+        if(isInt(siteId)){
+            StreamyAPI.onSubscribe(siteId.val());
+        } else {
+            alert("Give a valid number")
+        }
     });
+    var myEvent = new CustomEvent("userLogin",{detail:{username:"davidwalsh"}});
 });
 
-window.addEventListener('streamysubscribeerror', function() {
-    alert("Subscribe error! Check console for more information.");
+window.addEventListener('streamysubscribeerror', function(e) {
+    alert("Subscribe error! " + e.detail.errorMessage);
 });
 
-window.addEventListener('streamysubscribesucces', function() {
-    alert("Subscribe successful! Subscribe Id " + window.subscriptionId);
+window.addEventListener('streamysubscribesucces', function(e) {
+    alert("Subscribe successful! Subscribe Id " + e.detail.subscriptionId);
 });
