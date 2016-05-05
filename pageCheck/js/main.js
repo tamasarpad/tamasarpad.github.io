7e98107033e5ba7ec330c5f98823b33acfc1899d
Krasznai Valentin <krasznai.valentin@openminds.hu>
2016-05-05 13:25:39 +0200

    asd

--- pageCheck/js/main.js
+++ pageCheck/js/main.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
+	d();
+});
+
+$(window).resize(function(event) {
+	d();
+});
+
+function d(){
+	var x = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	var y = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 	$("#sX").text(screen.width);
 	$("#sY").text(screen.height);
 	$("#wX").text(window.width);
 	$("#wY").text(window.height);
 	$("#dX").text(document.width);
 	$("#dY").text(document.width);
-	$("#vX").text(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
-	$("#vY").text(window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
-});
+	$("#vX").text(x);
+	$("#vY").text(y);
+
+	console.log(x + " " + y);
+}
