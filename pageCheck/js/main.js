8b5a3caeebd2ae55dd6df0e848bbd42c0ab6b3cc
Krasznai Valentin <krasznai.valentin@openminds.hu>
2016-05-05 12:18:24 +0200

    asd

--- pageCheck/js/main.js
+++ pageCheck/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-	$("#sX").text("Screen width " + screen.width);
-	$("#sY").text("Screen height " + screen.height);
-	$("#wX").text("Window width " + window.width);
-	$("#wY").text("Window height " + window.height);
-	$("#dX").text("Document width " + document.width);
-	$("#dY").text("Document height " + document.width);
-	$("#vX").text("Viewport width " + window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
-	$("#vY").text("Viewport height " + window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
+	$("#sX").text(screen.width);
+	$("#sY").text(screen.height);
+	$("#wX").text(window.width);
+	$("#wY").text(window.height);
+	$("#dX").text(document.width);
+	$("#dY").text(document.width);
+	$("#vX").text(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+	$("#vY").text(window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
 });
