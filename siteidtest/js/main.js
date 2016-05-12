46e4a817db5fe0aef6ac2485d128c64c13dcac35
Valentin Krasznai <krasznai.valentin@openminds.hu>
2016-05-12 11:45:31 +0200

    asd

--- siteidtest/js/main.js
+++ siteidtest/js/main.js
@@ -1,17 +1,26 @@
-window.addEventListener('load', function() {
-    $("#button").click(function(event) {
-    	StreamyAPI.onSubscribe($("#siteId").val());
+var btnSub = $("#button");
+var inputSiteId = $("#siteId");
+var message = $("#message");
+
+$(document).ready(function() {
+    btnSub.click(function(event) {
+    	StreamyAPI.onSubscribe(inputSiteId.val());
+    });
+    
+    inputSiteId.change(function(event) {
+    	message.text('');
     });
 });
 
-window.addEventListener('streamysubscribeerror', function(e) {
+$(window).on('streamysubscribeerror', function(e) {
     alert("Subscribe error! " + e.detail.errorMessage);
-    $("#subId").text(e.detail.errorMessage);
+    message.text(e.detail.errorMessage);
     console.error(e.detail.errorMessage);
 });
 
-window.addEventListener('streamysubscribesuccess', function(e) {
+$(window).on('streamysubscribesuccess', function(e) {
     alert("Subscribe successful! " + e.detail.subscribeMessage);
-    $("#subId").text(e.detail.subscribeMessage);
+    message.text(e.detail.subscribeMessage);
     console.log(e.detail.subscribeMessage);
 });
+
