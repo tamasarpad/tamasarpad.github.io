7ca060fcaaf4968db04129d2aea8b084a210adb8
Valentin Krasznai <krasznai.valentin@openminds.hu>
2016-05-12 11:12:18 +0200

    asd

--- siteidtest/js/main.js
+++ siteidtest/js/main.js
@@ -6,6 +6,7 @@ window.addEventListener('load', function() {
 
 window.addEventListener('streamysubscribeerror', function(e) {
     alert("Subscribe error! " + e.detail.errorMessage);
+    $("#subId").text(e.detail.errorMessage);
     console.error(e.detail.errorMessage);
 });
 
