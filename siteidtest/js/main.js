81e3b6ab7787be0ab2cccfa74c34b9598db19683
Arpad Tamas <tamas.arpad@devmads.com>
2016-05-25 10:20:04 +0200

    asdasd

--- siteidtest/js/main.js
+++ siteidtest/js/main.js
@@ -12,37 +12,8 @@ $(document).ready(function() {
     });
 });
 
-$(window).on('onstreamynotificationssubscribeerror', function(e) {
-  //  message.css('color', 'red');
-    
-    // if (typeof e !== 'undefined' && e.detail && e.detail.message) {
-    //     message.text(e.detail.message);    
-    //     console.error("Error: " + e.detail.message);
-    // } else {
-    //     str = JSON.stringify(e, null, 4)
-    //     message.text(str);    
-    //     console.error("Error: " + str);
-        
-    // }
-    console.error('error');
-});
-
-$(window).on('onstreamynotificationssubscribesuccess', function(e) {
-    console.log('success');
-    // message.css('color', 'green');
-    // message.text(e.detail.message);
-    
-    // if (typeof e !== 'undefined' && e.detail && e.detail.message) {
-    //     message.text(e.detail.message);    
-    //     console.log("Success: " + e.detail.message);
-    // } else {
-    //     str = JSON.stringify(e, null, 4)
-    //     message.text(str);    
-    //     console.error("Success: " + str);  
-//    }  
-});
 
 (function(){
     var rgb = window.getComputedStyle(document.body, null).backgroundColor;
-    console.log(rgb+ 'asdasd');
+    console.log(rgb);
 })();
